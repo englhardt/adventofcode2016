3614f032651f47bdf6013cb9c3f289dda0468e87
englhardt <adrian.englhardt@gmail.com>
2016-12-15 11:23:16 +0100

    Add solution for day 15.

---
+++ 15/solve.jl
@@ -0,0 +1,19 @@
+data = map(x -> chop(x), readlines(open("input.txt")))
+data = hcat(map(x -> map(parse, matchall(r"\d+", x)), data)...).'
+
+pos = data[:, 4]
+range = data[:, 2]
+
+function simulate(pos)
+  t = 0
+  while !reduce(&, (pos + collect(0:length(pos) - 1)) .% range .== pos[1])
+    pos = (pos + 1) .% range
+    t += 1
+  end
+  println(t - 1)
+end
+
+simulate(pos)
+push!(pos, 0)
+push!(range, 11)
+simulate(pos)
